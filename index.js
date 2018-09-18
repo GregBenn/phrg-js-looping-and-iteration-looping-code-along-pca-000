c9d914d17ebf0e63d69ea7f1113d06b3a9b03db0
Greg Benn <gbenn07@gmail.com>
2018-09-18 11:22:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+function printBadges(employeeName) {
+  for (let i = 0; i < employeeName.length; i++) {
+    console.log(`Welcome ${employeeName[i]}! You are employee #${i + 1}.`);
+  }
+  return employeeName;
+}
+
+function tailsNeverFails() {
+  let numberOfCoinFlips = 0;
+
+  while (Math.random() >= 0.5) {
+    numberOfCoinFlips++;
+  }
+
+  return `You got ${numberOfCoinFlips} tails in a row!`;
+}
+
+// function flip() {
+//   let coinFlip;
+
+//   if (Math.random() >= 0.5) {
+//     return coinFlip = "Tails";
+//   } else {
+//     return coinFlip = "Heads";
+//   }
+// }
